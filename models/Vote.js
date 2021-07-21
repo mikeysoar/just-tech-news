4b54299dcc605308c8f37bd53c66b77bfce1b81f
Mikeysoar <mwclimb@gmail.com>
2021-07-21 13:59:23 -0600

    20210721-1359

--- models/Vote.js
+++ models/Vote.js
@@ -12,15 +12,13 @@ Vote.init(
         },
         user_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
             references: {
                 model: 'user',
                 key: 'id'
             }
         },
         post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
+            type: DataTypes.INTEGER,            
             references: {
                 model: 'post',
                 key: 'id'
